919017ec19d65795303848a21c8fbe9279ebf76d
cameron fukes <cfukes14@gmail.com>
2019-05-07 22:27:34 -0400

    Update store.php

--- store.php
+++ store.php
@@ -1,14 +1,11 @@
 <?php
-echo "hello";
 $inemail = $_POST['email'];
 $inpass = $_POST['password'];
-$inverify = $_POST['password2'];
 
 $domdick = new domDocument();
 $domdick->load("xmlstuff/users.xml");
 $CRLF = $domdick->createTextNode("\n");
-$root=$domdick->documentElement; // This can differ (I am not sure, it can be only documentElement or documentElement->firstChild or only firstChild)
-
+$root=$domdick->documentElement;
 
 $infotag = $domdick->createElement("user");
   $email = $domdick->createElement("email", $inemail);
@@ -22,16 +19,7 @@ $root->appendChild($CRLF);
 $root->appendChild($infotag);
 $root->appendChild($CRLF);
 
-  $guessedRight = true;
-if ($guessedRight)
-{
-header( 'Location: https://www.cs.sunyit.edu/~fukesc/listing_main.html' );
-die();
-}
-else
-{
-header( 'Location: https://www.cs.sunyit.edu/~fukesc/create.html' );
-}
+ $domdick->save('xmlstuff/users.xml');
 
-
+header( 'Location: https://www.cs.sunyit.edu/~fukesc/index.html' );
   ?>
